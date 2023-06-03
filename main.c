15ce7270e432f68fe5e4a6037caa949b0c6ab06d
Aaron Robson <5177808+AaronRobson@users.noreply.github.com>
2023-06-04 00:09:38 +0100

    manually inline the 'fizzBuzz' procedure

--- main.c
+++ main.c
@@ -1,25 +1,21 @@
 #include <stdio.h>
 
-void fizzBuzz(int value) {
-    short shouldPrintNumber = 1;
-    if (value % 3 == 0) {
-        printf("Fizz");
-        shouldPrintNumber = 0;
-    }
-    if (value % 5 == 0) {
-        printf("Buzz");
-        shouldPrintNumber = 0;
-    }
-    if (shouldPrintNumber) {
-        printf("%d", value);
-    }
-    printf("\n");
-}
-
 int main() {
     printf("Fizz Buzz in C:\n");
     for (int i = 1; i <= 100; i++) {
-        fizzBuzz(i);
+        short shouldPrintNumber = 1;
+        if (i % 3 == 0) {
+            printf("Fizz");
+            shouldPrintNumber = 0;
+        }
+        if (i % 5 == 0) {
+            printf("Buzz");
+            shouldPrintNumber = 0;
+        }
+        if (shouldPrintNumber) {
+            printf("%d", i);
+        }
+        printf("\n");
     }
     return 0;
 }
