c2837d8324a350ae431f0a024b9c0982f2a5e5ee
Aaron Robson <5177808+AaronRobson@users.noreply.github.com>
2023-08-17 04:02:46 +0100

    after further research stdbool looks to be just as efficient as a small int

--- main.c
+++ main.c
@@ -1,15 +1,16 @@
+#include <stdbool.h>
 #include <stdio.h>
 
 int main() {
     for (char i = 1; i <= 100; i++) {
-        char shouldPrintNumber = 1;
+        bool shouldPrintNumber = true;
         if (i % 3 == 0) {
             printf("Fizz");
-            shouldPrintNumber = 0;
+            shouldPrintNumber = false;
         }
         if (i % 5 == 0) {
             printf("Buzz");
-            shouldPrintNumber = 0;
+            shouldPrintNumber = false;
         }
         if (shouldPrintNumber) {
             printf("%d", i);
