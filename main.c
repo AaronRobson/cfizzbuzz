c80a82f5a49c30c1340191b37661a0613ce41c53
Aaron Robson <5177808+AaronRobson@users.noreply.github.com>
2023-08-22 03:09:34 +0100

    use 'uint8_t' as the loop variable as 'char' suggests a character

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 
 int main() {
-    for (char i = 1; i <= 100; i++) {
+    for (uint8_t i = 1; i <= 100; i++) {
         bool shouldPrintNumber = true;
         if (i % 3 == 0) {
             printf("Fizz");
